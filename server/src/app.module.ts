0045d62072db4b3e56333b76c590837e06372f44
hd1801 <hardikdhawan0108@gmail.com>
2022-05-05 12:25:53 +0530

    added modules

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PizzaModule } from './pizza/pizza.module';
+import { CartModule } from './cart/cart.module';
+import { OrderModule } from './order/order.module';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [PizzaModule, CartModule, OrderModule, UserModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })

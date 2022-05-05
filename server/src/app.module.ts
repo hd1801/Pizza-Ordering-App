7186122818c7d8905d3caff420f7d1adc6ff107e
hd1801 <hardikdhawan0108@gmail.com>
2022-05-05 12:38:07 +0530

    sequlize connection complete

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -6,9 +6,27 @@ import { CartModule } from './cart/cart.module';
 import { OrderModule } from './order/order.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
-  imports: [PizzaModule, CartModule, OrderModule, UserModule, AuthModule],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: '127.0.0.1',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'pizzastore',
+      synchronize: true,
+      autoLoadModels: true,
+      models: [],
+    }),
+    PizzaModule,
+    CartModule,
+    OrderModule,
+    UserModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

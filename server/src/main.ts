187d3d2e010a1a1874e1e66b63054cb48eec338e
hd1801 <hardikdhawan0108@gmail.com>
2022-05-07 17:57:39 +0530

    cors enabled

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,8 +3,8 @@ import { AppModule } from './app.module';
 import { ValidateInputPipe } from './core/validate.pipe';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidateInputPipe());
-  await app.listen(3000);
+  await app.listen(3333);
 }
 bootstrap();
